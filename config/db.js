f7810b87f1058d6fe8dfd6878d42bd4f35ad3d27
deknasa <80618997+deknasa@users.noreply.github.com>
2022-04-29 23:07:10 +0700

    delete comment

--- config/db.js
+++ config/db.js
@@ -1,16 +1,7 @@
 const Pool = require("pg").Pool;
 const config = require("./config")[process.env.NODE_ENV];
 
-
-// const pool = new Pool({
-//     user: "postgres",
-//     host: "localhost",
-//     database: "ReflectionAPI",
-//     password: "bolaliloq",
-//     port: 5432,
-// });
-
-
 const pool = new Pool(config);
 console.log(config);
+
 module.exports = pool;
