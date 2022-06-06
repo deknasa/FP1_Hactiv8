d59ee3a88a26707da0d6a21711658c9a7ff1b8dd
deknasa <80618997+deknasa@users.noreply.github.com>
2022-06-07 00:51:18 +0700

    ...

--- config/config.js
+++ config/config.js
@@ -11,11 +11,14 @@ module.exports = {
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
+      port: process.env.DB_PORT,
       host: process.env.DB_HOST,
       dialect: "postgres",
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
+      dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        }
+      }
     },
   }
